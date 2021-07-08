b9a23fb05816f40d57e001952e1b57462328dae7
Roman Tai <ruomengdai@microsoft.com>
2021-07-08 22:29:19 +0800

    feat: reverse sort

--- extension.js
+++ extension.js
@@ -1,22 +1,26 @@
 const vscode = require('vscode');
-
+const { compiler } = require("./lib/compiler")
 function activate(context) {
+	const task = function (desc) {
+		try {
+			const textEditor = vscode.window.activeTextEditor;
+			const selection = textEditor.selection;
+			const startLine = selection.start.line;
+			const startPos = selection.start.character;
+			const endLine = selection.end.line;
+			const endPos = selection.end.character;
+			const selectedText = getSelection(textEditor, startLine, startPos, endLine, endPos);
+			const sortedText = compiler(selectedText, desc);
+			setSelection(textEditor, startLine, startPos, endLine, endPos, sortedText);
+			return true;
+		} catch (e) { void 0; }
+	}
 	const commands = [
-		vscode.commands.registerCommand("roman.sortJS", function () {
-			try {
-				const textEditor = vscode.window.activeTextEditor;
-				const selection = textEditor.selection;
-				const startLine = selection.start.line;
-				const startPos = selection.start.character;
-				const endLine = selection.end.line;
-				const endPos = selection.end.character;
-				const selectedText = getSelection(textEditor, startLine, startPos, endLine, endPos);
-				const sortedText = "---------------------------------------------";
-				setSelection(textEditor, startLine, startPos, endLine, endPos, selectedText + sortedText);
-				return true;
-			} catch (e) { void 0; }
-		})];
+		vscode.commands.registerCommand("roman.sort", () => task()),
+		vscode.commands.registerCommand("roman.sort.reverse", () => task(true))
+	];
 
+	// context.subscriptions.push(disposable);
 	commands.map((command) => context.subscriptions.push(command));
 }
 
